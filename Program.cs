a7e25c74d387b6c65756c7af170b5a45db219621
TigerDK <simon260704@gmail.com>
2026-09-07 12:54:15 +0200

    Noter

--- Program.cs
+++ Program.cs
@@ -5,20 +5,57 @@
         static void Main(string[] args)
         {
             int førstOkt;
+            string input;
+
+            /* [] betyder at man arbejder med flere værdier i et array
+             * FX:
+             * string[] ipDele = { "192", "168", "1", "50" };
+             * ipDele[0] = "192"
+             * ipDele[1] = "168"
+             * ipDele[2] = "1"
+             * ipDele[3] = "50"
+             */
+            // Her afgøre vi, at denne string arbejder med et array
+            string[] ipDele;
 
         begyndelse:
 
             Console.WriteLine("Oktet Analyser");
-            Console.Write("Indtast din første oktet:");
-            førstOkt = Convert.ToInt32(Console.ReadLine());
 
+            Console.Write("Indtast første oktet eller fuld IP-adresse: ");
+            input = Console.ReadLine();
+
+            //Her giver vi brugeren mulighed for at skrive enten en fuld IP-adresse eller kun den første oktet
+            // .Contains er en funktion, der bruges til at finde bestemte tegn, ord eller sætninger i en string
+            if (input.Contains("."))
+            {
+                /*Her splitter vi inputtet op ved hvert punktum. Da ipDele er et string array
+                 * bliver de forskellige dele af ip adressen gemt som separate værdier i arrayet
+                 */
+                ipDele = input.Split('.');
+                /* Her konverterer vi den første del fra string til et objekt, så værdien kan gemmes i "førstOkt"
+                 * Det er også her, vi vælger hvilken del af arrayet der skal bruges
+                 * ipDele[0] = første del af arrayet
+                 * ipDele[1] = anden del
+                 * ipDele[2] tredje del
+                 * Osv.
+                */
+                førstOkt = Convert.ToInt32(ipDele[0]);
+            }
+            else
+            {
+                // Vis der ikke er noget '.' der med ikke en fuld IP-adresse går den videre som en Oktet
+                førstOkt = Convert.ToInt32(input);
+            }
+
+            // Her laver vi et objekt af IP-klassen, så vi kan bruge funktionerne inde i klassen
             IP ip = new IP();
 
+            // Dette kalder dataen som er i Class, de forskellige pulic strings
             string ipKlasse = ip.FindKlasse(førstOkt);
             string subnetMaske = ip.FindSubnetMask(førstOkt);
 
-            // false statement ved brug af !
-
+            // ! tjekker om betingelsen er false, som vi bruger til at tjekke om oktetten er ugyldig
             if (!(førstOkt >= 1 && førstOkt <= 255))
             {
                 Console.WriteLine("Ugyldig oktet");
@@ -44,6 +81,10 @@
 
 
         }
+
+        /* Vi valgte at bruge Class og public strings..........
+         * 
+         */
         class IP
         {
             public string FindKlasse(int førstOkt)
