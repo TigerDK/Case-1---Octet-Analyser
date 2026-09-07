6d9cab662f33b891213336a5f3c7506a29af585f
TigerDK <116790310+TigerDK@users.noreply.github.com>
2026-09-07 09:24:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Case_1___Octet_Analyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+        }
+    }
+}
