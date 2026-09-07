2fb43b5f49aed3ce19cab4f2d05cf697fe54567f
TigerDK <simon260704@gmail.com>
2026-09-07 11:57:43 +0200

    Konklusion & Edits
    Lavede konklusion.
    Gjorde if statements mindre.
    Stave ændringer.

--- Program.cs
+++ Program.cs
@@ -4,99 +4,107 @@
     {
         static void Main(string[] args)
         {
-            int oktet;
             int førstOkt;
 
-            IP ip = new IP();
-
-            ip.FindKlasse(førstOkt);
-            ip.FindSubnetMask(førstOkt);
-
+        begyndelse:
 
             Console.WriteLine("Oktet Analyser");
             Console.Write("Indtast din første oktet:");
             førstOkt = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Din oktet er {0}", førstOkt);
 
+            IP ip = new IP();
 
-            
+            string ipKlasse = ip.FindKlasse(førstOkt);
+            string subnetMaske = ip.FindSubnetMask(førstOkt);
 
+            if (!(førstOkt >= 1 && førstOkt <= 255))
+            {
+                Console.WriteLine("Ugyldig oktet");
+                Console.ReadKey();
+                Console.Clear();
 
-
+                goto begyndelse;
+            }
+            else
+            {
+                Console.WriteLine("--------------------------------------------");
+                Console.WriteLine("             Konklusion");
+                Console.WriteLine("--------------------------------------------");
+                Console.WriteLine("Din oktet {0} svarer til", førstOkt);
+                Console.WriteLine();
+                Console.WriteLine("IP-Klass: {0}", ipKlasse);
+                Console.WriteLine();
+                Console.WriteLine("Default subnetmaske: {0}", subnetMaske);
+                Console.WriteLine("--------------------------------------------");
+                Console.WriteLine("          Tryk for at afslutte");
+                Console.WriteLine("--------------------------------------------");
+            }
 
 
         }
         class IP
         {
-            public void FindKlasse(int førstOkt)
+            public string FindKlasse(int førstOkt)
             {
-                int ip_class;
-                int ip_class_a;
-                int ip_class_b;
-                int ip_class_c;
-                int ip_loop_back = 127;
-                
+                string ip_klasse = "";
+
                 /* a class <= 0 && >= 127 
                  * b class <= 128 && >= 191 
                  * c class <= 192 && >= 223 
                  * (0-127 A, 128-191 B, 192-223 C, 224-239 D, 240-255 E)
                  */
-                begyndelse:
+
                 if (førstOkt >= 1 && førstOkt <= 126)
                 {
-                    Console.WriteLine("Din indtasted oktet {0} ligger i A-klassen", førstOkt);
-                }
-                else if (førstOkt >= 128 && førstOkt <= 191)
-                {
-                    Console.WriteLine("Din indtasted oktet {0} ligger i B-klassen", førstOkt);
-                }
-                else if (førstOkt >= 192 && førstOkt <= 223)
-                {
-                    Console.WriteLine("Din indtasted oktet {0} ligger i C-klassen", førstOkt);
+                    ip_klasse = "A Klasse";
                 }
                 else if (førstOkt == 127)
                 {
-                    Console.WriteLine("Din indtasted oktet {0} er en loopback og ikke en brugbar oktet", førstOkt);
-                }
-                else if (førstOkt >= 224 && førstOkt <= 255)
-                {
-                    Console.WriteLine("Din indtasted oktet {0} er enten en D eller E Klasse", førstOkt);
-                    Console.WriteLine("Tryk for at indtaste en ny oktet");
-                    goto begyndelse;
-
-                }
-                else
-                {
-                    Console.WriteLine("Din indtasted oktet {0} er en ugyldig oktet Tryk for at prøv igen", førstOkt);
-                    goto begyndelse;
-                }
-
-
-
-            }
-            public void FindSubnetMask(int førstOkt)
-            {
-                int subnetdefault;
-                int subnetcustom;
-                
-                if (førstOkt >= 1 && førstOkt <= 126)
-                {
-                    Console.WriteLine("Standard subnetmaske: 255.0.0.0");
+                    ip_klasse = "Loopback";
                 }
                 else if (førstOkt >= 128 && førstOkt <= 191)
                 {
-                    Console.WriteLine("Standard subnetmaske: 255.255.0.0");
+                    ip_klasse = "B Klasse";
                 }
                 else if (førstOkt >= 192 && førstOkt <= 223)
                 {
-                    Console.WriteLine("Standard subnetmaske: 255.255.255.0");
+                    ip_klasse = "C Klasse";
+                }
+                else if (førstOkt >= 224 && førstOkt <= 255)
+                {
+                    ip_klasse = "D eller E Klasse";
+                }
+
+                return ip_klasse;
+            }
+            public string FindSubnetMask(int førstOkt)
+            {
+                string subnetdefault;
+
+                if (førstOkt >= 1 && førstOkt <= 126)
+                {
+                    subnetdefault = "255.0.0.0";
+                }
+                else if (førstOkt >= 128 && førstOkt <= 191)
+                {
+                    subnetdefault = "255.255.0.0";
+                }
+                else if (førstOkt >= 192 && førstOkt <= 223)
+                {
+                    subnetdefault = "255.255.255.0";
+                }
+                else if (førstOkt == 127)
+                {
+                    subnetdefault = "Loopback har ingen standard subnetmaske";
                 }
                 else
                 {
-                    Console.WriteLine("Ingen standard subnetmaske");
+                    subnetdefault = "har ikke en subnetmaske";
                 }
-            }
 
+                return subnetdefault;
+
+            }
         }
     }
 }
