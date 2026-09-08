1840e3cb26727678ad81da07d689856f51bb3663
TigerDK <simon260704@gmail.com>
2026-09-08 08:15:00 +0200

    Fjernede noter
    Noter der ikke skulle være i den endelige aflevering

--- Program.cs
+++ Program.cs
@@ -66,18 +66,18 @@
             }
             else
             {
-                // Lavet af Lucas
-                Console.WriteLine("--------------------------------------------");      //
-                Console.WriteLine("             Konklusion");                           //
-                Console.WriteLine("--------------------------------------------");      //
-                Console.WriteLine("Din oktet {0} svarer til", førstOkt);                //
-                Console.WriteLine();                                                    //
-                Console.WriteLine("IP-Klass: {0}", ipKlasse);                           //
-                Console.WriteLine();                                                    //
-                Console.WriteLine("Default subnetmaske: {0}", subnetMaske);             //
-                Console.WriteLine("--------------------------------------------");      //
-                Console.WriteLine("          Tryk for at afslutte");                    //
-                Console.WriteLine("--------------------------------------------");      //
+                
+                Console.WriteLine("--------------------------------------------");      
+                Console.WriteLine("             Konklusion");                           
+                Console.WriteLine("--------------------------------------------");      
+                Console.WriteLine("Din oktet {0} svarer til", førstOkt);                
+                Console.WriteLine();                                                    
+                Console.WriteLine("IP-Klass: {0}", ipKlasse);                           
+                Console.WriteLine();                                                    
+                Console.WriteLine("Default subnetmaske: {0}", subnetMaske);             
+                Console.WriteLine("--------------------------------------------");      
+                Console.WriteLine("          Tryk for at afslutte");                    
+                Console.WriteLine("--------------------------------------------");      
             }
 
 
@@ -98,55 +98,55 @@
                  * (0-127 A, 128-191 B, 192-223 C, 224-239 D, 240-255 E)
                  */
 
-                // Lavet af lucas og modificeret af Simon
-                if (førstOkt >= 1 && førstOkt <= 126)   //
-                {                                       //
-                    ip_klasse = "A Klasse";             //
-                }                                       //
-                else if (førstOkt == 127)               //
-                {                                       //
-                    ip_klasse = "Loopback";             //
-                }                                       //
-                else if (førstOkt >= 128 && førstOkt <= 191)    //
-                {                                       //
-                    ip_klasse = "B Klasse";             //
-                }                                       //
-                else if (førstOkt >= 192 && førstOkt <= 223)    //
-                {                                       //
-                    ip_klasse = "C Klasse";             //
-                }                                       //
-                else if (førstOkt >= 224 && førstOkt <= 255)    //
-                {                                       //
-                    ip_klasse = "D eller E Klasse";     //
-                }                                       //
+                
+                if (førstOkt >= 1 && førstOkt <= 126)   
+                {                                       
+                    ip_klasse = "A Klasse";             
+                }                                       
+                else if (førstOkt == 127)               
+                {                                       
+                    ip_klasse = "Loopback";             
+                }                                       
+                else if (førstOkt >= 128 && førstOkt <= 191)    
+                {                                       
+                    ip_klasse = "B Klasse";             
+                }                                       
+                else if (førstOkt >= 192 && førstOkt <= 223)    
+                {                                       
+                    ip_klasse = "C Klasse";             
+                }                                       
+                else if (førstOkt >= 224 && førstOkt <= 255)    
+                {                                       
+                    ip_klasse = "D eller E Klasse";     
+                }                                       
 
                 return ip_klasse;
             }
             public string FindSubnetMask(int førstOkt)
             {
-                // Lavet af lucas og modificeret af Simon
-                string subnetdefault;                               //
-                                                                    //
-                if (førstOkt >= 1 && førstOkt <= 126)               //
-                {                                                   //
-                    subnetdefault = "255.0.0.0";                    //
-                }                                                   //
-                else if (førstOkt >= 128 && førstOkt <= 191)        //
-                {                                                   //
-                    subnetdefault = "255.255.0.0";                  //
-                }                                                   //
-                else if (førstOkt >= 192 && førstOkt <= 223)        //
-                {                                                   //
-                    subnetdefault = "255.255.255.0";                //
-                }                                                   //
-                else if (førstOkt == 127)                           //
-                {                                                   //
-                    subnetdefault = "Loopback har ingen standard subnetmaske";      //
-                }                                                   //
-                else                                                //                                
-                {                                                   //
-                    subnetdefault = "har ikke en subnetmaske";      //
-                }                                                   //
+                
+                string subnetdefault;                               
+                                                                    
+                if (førstOkt >= 1 && førstOkt <= 126)               
+                {                                                   
+                    subnetdefault = "255.0.0.0";                    
+                }                                                   
+                else if (førstOkt >= 128 && førstOkt <= 191)        
+                {                                                   
+                    subnetdefault = "255.255.0.0";                  
+                }
+                else if (førstOkt >= 192 && førstOkt <= 223)
+                {
+                    subnetdefault = "255.255.255.0";
+                }
+                else if (førstOkt == 127)
+                {
+                    subnetdefault = "Loopback har ingen standard subnetmaske";
+                }                                                   
+                else
+                {
+                    subnetdefault = "har ikke en subnetmaske";
+                }
 
                 return subnetdefault;
 
